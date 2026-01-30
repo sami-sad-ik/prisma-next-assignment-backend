07aad31e9b55791c852b0a22ef4d74a13249d09f
sami-sad-ik <sadiksami530@gmail.com>
2026-01-31 00:46:35 +0600

    better-auth default user

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,10 @@
 import express from "express";
+import { toNodeHandler } from "better-auth/node";
+import { auth } from "./lib/auth";
 
 const app = express();
+
+app.all("/api/auth/*splat", toNodeHandler(auth));
+app.use(express.json());
 
 export default app;
